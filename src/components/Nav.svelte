8c767ad60d3af80c717aa9b5b2b94756bafd3b03
v-rogg <itsabattleship@gmail.com>
2020-03-26 15:17:03 +0100

    Fix nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -92,7 +92,7 @@
         {#if $lang === 'en'}
             <li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>Bio</a></li>
         {/if}
-        <li><a aria-current='{segment === "about" ? "repertoire" : undefined}' href='repertoire'>Repertoire</a></li>
+        <li><a aria-current='{segment === "repertoire" ? "page" : undefined}' href='repertoire'>Repertoire</a></li>
         <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
              the blog data when we hover over the link or tap it on a touchscreen -->
         		<li><a rel=prefetch aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>What did I teach my Mom today?</a></li>
