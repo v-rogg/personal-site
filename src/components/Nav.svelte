b1c6d780b73d94666c6af5a17b4d96c0900dc5b6
v-rogg <itsabattleship@gmail.com>
2020-03-26 15:14:36 +0100

    Add repertoire page

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -87,11 +87,12 @@
             <li><a aria-current='{segment === undefined ? "page" : undefined}' href='.'>Projects</a></li>
         {/if}
         {#if $lang === 'de'}
-            <li><a aria-current='{segment === "about" ? "page" : undefined}' href='.'>Bio</a></li>
+            <li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>Bio</a></li>
         {/if}
         {#if $lang === 'en'}
             <li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>Bio</a></li>
         {/if}
+        <li><a aria-current='{segment === "about" ? "repertoire" : undefined}' href='repertoire'>Repertoire</a></li>
         <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
              the blog data when we hover over the link or tap it on a touchscreen -->
         		<li><a rel=prefetch aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>What did I teach my Mom today?</a></li>
