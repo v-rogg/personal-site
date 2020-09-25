80c7cf1097f3b1e81f60bfcc19dfbee6114cfb58
v-rogg <itsabattleship@gmail.com>
2020-09-25 04:56:32 +0200

    Delete unused stuff

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,43 +5,6 @@
 			font-size: 2rem;
 		}
 	}
-
-	body {
-		margin: 0;
-		font-family: Roboto, -apple-system, BlinkMacSystemFont, Segoe UI, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-		font-size: 14px;
-		line-height: 1.5;
-		color: #333;
-	}
-
-	h1, h2, h3, h4, h5, h6 {
-		margin: 0 0 0.5em 0;
-		font-weight: 400;
-		line-height: 1.2;
-	}
-
-	h1 {
-		font-size: 2em;
-	}
-
-	a {
-		color: inherit;
-	}
-
-	code {
-		font-family: menlo, inconsolata, monospace;
-		font-size: calc(1em - 2px);
-		color: #555;
-		background-color: #f0f0f0;
-		padding: 0.2em 0.4em;
-		border-radius: 2px;
-	}
-
-	@media (min-width: 400px) {
-		body {
-			font-size: 16px;
-		}
-	}
 </style>
 
 <svelte:head>
@@ -52,7 +15,7 @@
 <section>
 	<h1>Great success!</h1>
 
-	<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+	<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading. <i class="mi-heart"></i></strong></p>
 </section>
 
 <div>
