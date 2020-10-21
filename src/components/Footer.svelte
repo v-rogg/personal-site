2c100a8afa7a48ad81eb58e11c099eeae0318b5c
v-rogg <itsabattleship@gmail.com>
2020-10-21 22:31:41 +0200

    Update date

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -51,7 +51,7 @@
 <footer class="container">
     <div id="signature">
         <img src="assets/signature.svg" alt="Unterschrift Valentin Rogg">
-        Stand 07.10.20
+        Stand 21.10.20
     </div>
     <div id="social-media">
         <a href="https://www.linkedin.com/in/vrogg/" aria-label="LinkedIn">
