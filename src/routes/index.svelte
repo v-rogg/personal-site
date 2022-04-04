b085aa1cf68edd45e9086ed1894b931b6a1fad7b
Valentin Rogg <valentin.rogg@xentral.com>
2022-04-05 00:20:01 +0200

    Switch to neo (#1)
    * Remove old
    
    * Init new sveltekit project
    
    * Reformat files

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,32 +1,59 @@
-<script>
-    import CV from "./main/CV.svelte";
-    import Skills from "./main/Skills.svelte";
-    import Projects from "./main/Projects.svelte";
+<script context="module" lang="ts">
+  export const prerender = true;
 </script>
 
-<style lang="scss" global>
-    .split {
-        display: grid;
-        grid-template-columns: 40% 60%;
+<script lang="ts">
+  import Counter from '$lib/Counter.svelte';
+</script>
 
-        //.left {
-        //    width: 40%;
-        //}
+<svelte:head>
+  <title>Home</title>
+</svelte:head>
 
-        //.rit {
-        //    width: 60%;
-        //}
-    }
+<section>
+  <h1>
+    <div class="welcome">
+      <picture>
+        <source srcset="svelte-welcome.webp" type="image/webp" />
+        <img src="svelte-welcome.png" alt="Welcome" />
+      </picture>
+    </div>
 
-    @media (max-width: 991.98px) {
-        .split {
-            grid-template-columns: 100%;
-        }
-    }
+    to your new<br />SvelteKit app
+  </h1>
+
+  <h2>
+    try editing <strong>src/routes/index.svelte</strong>
+  </h2>
+
+  <Counter />
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+  }
+
+  h1 {
+    width: 100%;
+  }
+
+  .welcome {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding: 0 0 calc(100% * 495 / 2048) 0;
+  }
+
+  .welcome img {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    display: block;
+  }
 </style>
-
-<div class="split container">
-    <CV />
-    <Projects />
-    <Skills />
-</div>
