60b79fb0c232d626d6e08d1968e25642f007b256
Valentin Rogg <valentin.rogg@xentral.com>
2022-04-05 21:06:45 +0200

    New format (#2)
    * Remove old
    
    * Init new sveltekit project
    
    * Reformat files
    
    * New format
    
    * New format

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,35 +1,44 @@
 <script lang="ts">
   import { page } from '$app/stores';
-  import logo from './svelte-logo.svg';
+
+  let lang = 0;
+  const languages = ["EN", "DE"];
+
+  let invert = false;
+
+  function toggle_lang() {
+    if (lang + 1 < languages.length) {
+      lang++
+    } else {
+      lang = 0;
+    }
+  }
 </script>
 
-<header>
-  <div class="corner">
-    <a href="https://kit.svelte.dev">
-      <img src={logo} alt="SvelteKit" />
+<header class="container">
+  <h1 class="">
+    <a href="/">
+      Valentin Rogg
     </a>
-  </div>
+  </h1>
 
   <nav>
-    <svg viewBox="0 0 2 3" aria-hidden="true">
-      <path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-    </svg>
     <ul>
-      <li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-      <li class:active={$page.url.pathname === '/about'}>
-        <a sveltekit:prefetch href="/about">About</a>
+      <li class:active={$page.url.pathname === '/'}>
+        <a sveltekit:prefetch href="/">Home</a>
       </li>
-      <li class:active={$page.url.pathname === '/todos'}>
-        <a sveltekit:prefetch href="/todos">Todos</a>
+      <li class:active={$page.url.pathname === '/blog'} class="strikethrough">
+        <a sveltekit:prefetch href="/" aria-disabled="true">Blog</a>
+      </li>
+      <li class:active={$page.url.pathname === '/projects'} class="strikethrough">
+        <a sveltekit:prefetch href="/" aria-disabled="true">Projects</a>
       </li>
     </ul>
-    <svg viewBox="0 0 2 3" aria-hidden="true">
-      <path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-    </svg>
   </nav>
 
-  <div class="corner">
-    <!-- TODO put something else here? github link? -->
+  <div class="options">
+    <button on:click={() => {invert = !invert}} class:invert={invert} class="dark_switch"><i class="fak fa-moon"></i></button>
+    <button on:click={() => {toggle_lang()}} class="language_switch">{languages[lang]}</button>
   </div>
 </header>
 
@@ -37,88 +46,57 @@
   header {
     display: flex;
     justify-content: space-between;
-  }
-
-  .corner {
-    width: 3em;
-    height: 3em;
-  }
-
-  .corner a {
-    display: flex;
     align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
+    font-size: 16px;
+    margin-bottom: 300px;
   }
 
-  .corner img {
-    width: 2em;
-    height: 2em;
-    object-fit: contain;
-  }
-
-  nav {
+  nav ul {
     display: flex;
-    justify-content: center;
-    --background: rgba(255, 255, 255, 0.7);
+    gap: 2rem;
   }
 
-  svg {
-    width: 2em;
-    height: 3em;
-    display: block;
-  }
-
-  path {
-    fill: var(--background);
-  }
-
-  ul {
-    position: relative;
-    padding: 0;
-    margin: 0;
-    height: 3em;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  nav ul li {
     list-style: none;
-    background: var(--background);
-    background-size: contain;
   }
 
-  li {
-    position: relative;
-    height: 100%;
-  }
-
-  li.active::before {
-    --size: 6px;
-    content: '';
-    width: 0;
-    height: 0;
-    position: absolute;
-    top: 0;
-    left: calc(50% - var(--size));
-    border: var(--size) solid transparent;
-    border-top: var(--size) solid var(--accent-color);
-  }
-
-  nav a {
+  .options {
     display: flex;
-    height: 100%;
     align-items: center;
-    padding: 0 1em;
-    color: var(--heading-color);
-    font-weight: 700;
-    font-size: 0.8rem;
-    text-transform: uppercase;
-    letter-spacing: 0.1em;
-    text-decoration: none;
-    transition: color 0.2s linear;
+    gap: 16px;
   }
 
-  a:hover {
-    color: var(--accent-color);
+  .dark_switch {
+    border: none;
+    background: none;
+    margin: 0;
+    padding: 0;
+    font-size: 16px;
+  }
+
+  .dark_switch:hover {
+    cursor: pointer;
+  }
+
+  .language_switch {
+    width: 50px;
+    height: 24px;
+    border-radius: 4px;
+    border: none;
+    background: var(--lightgrey);
+  }
+
+  .language_switch:hover {
+    background: var(--mediumgrey);
+    cursor: pointer;
+  }
+
+  .strikethrough {
+    text-decoration: line-through;
+  }
+
+  h1 {
+    font-family: "Pulp Display", sans-serif;
+    font-size: 18px;
   }
 </style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,45 +1,29 @@
 <script lang="ts">
   import Header from '$lib/header/Header.svelte';
   import '../app.css';
+  import Footer from '../lib/footer/Footer.svelte';
 </script>
 
-<Header />
+<div class="dark">
+  <Header />
 
-<main>
-  <slot />
-</main>
+  <main>
+    <slot />
+  </main>
 
-<footer>
-  <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
+  <Footer />
+</div>
 
 <style>
-  main {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    padding: 1rem;
-    width: 100%;
-    max-width: 1024px;
-    margin: 0 auto;
-    box-sizing: border-box;
+  :global(a) {
+    text-decoration: none;
+    color: var(--black);
   }
 
-  footer {
+  div {
+    height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: center;
-    padding: 40px;
-  }
-
-  footer a {
-    font-weight: bold;
-  }
-
-  @media (min-width: 480px) {
-    footer {
-      padding: 40px 0;
-    }
   }
 </style>
