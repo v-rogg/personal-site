9bcbb7a2ae480a44c4553886406e60df40217859
Valentin Rogg <valentin.rogg@xentral.com>
2022-04-06 13:46:51 +0200

    Add working dark mode (#3)
    * Remove old
    
    * Init new sveltekit project
    
    * Reformat files
    
    * New format
    
    * New format
    
    * Lint
    
    * Remove deprecated

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -1,12 +1,12 @@
 <footer class="container">
   <div class="legal">
-    Legal <br/>
-    <i class="fa-regular fa-copyright"></i> 4/5/21
+    <div>Legal</div>
+    <div class="copy"><i class="fa-regular fa-copyright" /><span><span>4</span>/<span>5</span>/<span>21</span></span></div>
   </div>
 
   <div class="links">
-    <a href="https://www.linkedin.com/in/vrogg/"><i class="fa-brands fa-linkedin"></i></a>
-    <a href="https://github.com/v-rogg"><i class="fa-brands fa-github"></i></a>
+    <a href="https://www.linkedin.com/in/vrogg/"><i class="fa-brands fa-linkedin" /></a>
+    <a href="https://github.com/v-rogg"><i class="fa-brands fa-github" /></a>
   </div>
 </footer>
 
@@ -16,12 +16,31 @@
     justify-content: space-between;
     align-items: flex-end;
     font-size: 16px;
-    margin-top: 300px;
   }
 
   .links {
     font-size: 18px;
     display: flex;
     gap: 16px;
+  }
+
+  .legal {
+    display: flex;
+    gap: 16px;
+    align-items: center;
+  }
+
+  .copy {
+    display: flex;
+    gap: 13px;
+    align-items: center;
+  }
+
+  .copy span {
+    font-size: 14px;
+  }
+
+  .copy span span {
+    margin: 0 3px;
   }
 </style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,23 +1,33 @@
 <script lang="ts">
-  import Header from '$lib/header/Header.svelte';
-  import '../app.css';
-  import Footer from '../lib/footer/Footer.svelte';
+  import Header from "$lib/header/Header.svelte";
+  import "../app.css";
+  import Footer from "../lib/footer/Footer.svelte";
+  import { darkmode } from "../stores.ts";
+  import CenterLayout from "../lib/SpecialLayouts/CenterLayout.svelte";
 </script>
 
-<div class="dark">
-  <Header />
-
-  <main>
-    <slot />
-  </main>
-
-  <Footer />
+<div class="background" class:light={!$darkmode} class:dark={$darkmode}>
+  <CenterLayout>
+    <Header slot="top_mid" />
+    <slot slot="center" />
+    <Footer slot="bottom_mid" />
+  </CenterLayout>
 </div>
 
 <style>
+  .background {
+    background: var(--c-white);
+    color: var(--c-black) !important;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
   :global(a) {
     text-decoration: none;
-    color: var(--black);
+    color: var(--c-black);
   }
 
   div {
