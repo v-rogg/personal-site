ab3790cd563a4504cc2ed67454a10138b4d0bd4c
v-rogg <mail@valentinrogg.de>
2023-10-09 02:08:27 +0200

    Fix production env

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,7 @@ WORKDIR /app
 
 # Set production environment
 ENV NODE_ENV="production"
+ENV PUBLIC_NODE_ENV="production"
 
 # Throw-away build stage to reduce size of final image
 FROM base as build
--- src/routes/(public)/[lang]/+page.ts
+++ src/routes/(public)/[lang]/+page.ts
@@ -1,12 +1,15 @@
 import type { PageLoad } from "./$types";
+import { PUBLIC_NODE_ENV } from "$env/static/public";
 
 export const prerender = false;
 
 export const load: PageLoad = async ({ data, parent, params }) => {
 	const { storyblokApi } = await parent();
 
+	console.log(PUBLIC_NODE_ENV);
+
 	const cv = await storyblokApi.get(`cdn/stories/cv/${params.lang}`, {
-		version: "draft",
+		version: PUBLIC_NODE_ENV === "production" ? "published" : "draft",
 	});
 
 	return {
