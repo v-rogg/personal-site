2e423e4c55c0d93e641a511ea83b5987ee793860
v-rogg <mail@valentinrogg.de>
2023-10-29 19:50:18 +0100

    Update workflow

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,6 +1,6 @@
 import { pgTable, pgEnum, uuid, text, timestamp, boolean, json } from "drizzle-orm/pg-core"
-
 import { sql } from "drizzle-orm"
+
 export const keyStatus = pgEnum("key_status", ['expired', 'invalid', 'valid', 'default'])
 export const keyType = pgEnum("key_type", ['stream_xchacha20', 'secretstream', 'secretbox', 'kdf', 'generichash', 'shorthash', 'auth', 'hmacsha256', 'hmacsha512', 'aead-det', 'aead-ietf'])
 export const factorType = pgEnum("factor_type", ['webauthn', 'totp'])
