9e94833ef64b60c1576ecf0feed7b1337766809e
v-rogg <mail@valentinrogg.de>
2023-10-30 10:54:21 +0100

    Fix not null field

--- supabase/migrations/20231024120552_init_signatures_sqlite.sql
+++ supabase/migrations/20231024120552_init_signatures_sqlite.sql
@@ -1,6 +1,6 @@
 drop table if exists signatures;
 create table if not exists signatures (
-    id          text primary key,
+    id          text primary key not null,
     name        text   not null,
     ts_created  text default current_timestamp not null,
     ts_modified text,
