ca01cd63b0fbfc6c8d33c843eeec079d4a79c6cc
v-rogg <mail@valentinrogg.de>
2023-11-04 14:53:59 +0100

    Fix tags in darkmode

--- src/lib/components/globals/Tag/Tag.svelte
+++ src/lib/components/globals/Tag/Tag.svelte
@@ -7,10 +7,10 @@
 <!--</div>-->
 
 <div
-	class="relative z-10 pointer-events-none backdrop-blur-md px-2 py-1 rounded-lg ring-1 ring-inset ring-current flex items-center overflow-hidden" style="color: {color}">
-	<div class="absolute top-0 left-0 bg-current opacity-10" style="width: 100%; height: 100%">
+	class="relative z-10 pointer-events-none backdrop-blur-md px-2 py-1 rounded-lg ring-1 ring-inset ring-current flex items-center overflow-hidden dark:ring-white-500" style="color: {color}">
+	<div class="absolute top-0 left-0 bg-current dark:bg-white-500 opacity-10" style="width: 100%; height: 100%">
 	</div>
-	<div class="text-xs" style="color: {color}">
+	<div class="text-xs dark:text-white-500">
 		<slot/>
 	</div>
 </div>
