736da4404972246bc33f48313632af5fceae8550
v-rogg <mail@valentinrogg.de>
2023-11-22 19:10:26 +0100

    Update fetch logging

--- src/lib/connection/fetch.pulic.ts
+++ src/lib/connection/fetch.pulic.ts
@@ -1,6 +1,9 @@
 export async function get<T>(url: RequestInfo | URL, f = fetch): Promise<T> {
 	return f(url).then(async (res) => {
 		console.log(res.status, res.statusText);
+		if (res.status != 200) {
+			console.log(res.text());
+		}
 		return res.json()
 	});
 }
