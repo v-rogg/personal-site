6f7316ae602d104263acb5f10b4e9b1e23cf8746
v-rogg <mail@valentinrogg.de>
2024-12-14 01:58:32 +0100

    Update error page

--- src/lib/components/globals/Header.svelte
+++ src/lib/components/globals/Header.svelte
@@ -35,30 +35,36 @@
 					<img src="/favicon.svg" alt="vr" width="56" height="56" />
 				</a>
 			{/if}
-			<div class="flex items-center gap-1">
-				{#each slugs as page, i}
-					{@const name = page.split("/").at(-1) || ""}
-					{@const delay = 100}
-					{@const duration = 500}
-					{@const amount = 10}
-					<div
-						id={i + ""}
-						in:fly={{ delay: i * delay, duration, amount, easing: expoOut }}
-						out:fly={{ delay: (slugs.length + 1 - i) * delay, duration, amount, easing: expoIn }}
-					>
-						<i class="fa-solid fa-slash-forward"></i>
-						{#if slugs.length - 1 === i}
-							<span class="mx-1 rounded px-1 py-0.5">
-								{name.charAt(0).toUpperCase() + name.slice(1)}
-							</span>
-						{:else}
-							<a href={page} class="mx-1 rounded px-1 py-0.5 hover:bg-white-700 active:bg-white-600">
-								{name.charAt(0).toUpperCase() + name.slice(1)}
-							</a>
-						{/if}
-					</div>
-				{/each}
-			</div>
+			{#if $page.status === 200}
+				<div class="flex items-center gap-1">
+					{#each slugs as page, i}
+						{@const name = page.split("/").at(-1) || ""}
+						{@const delay = 100}
+						{@const duration = 500}
+						{@const amount = 10}
+						<div
+							id={i + ""}
+							in:fly={{ delay: i * delay, duration, amount, easing: expoOut }}
+							out:fly={{ delay: (slugs.length + 1 - i) * delay, duration, amount, easing: expoIn }}
+						>
+							<i class="fa-solid fa-slash-forward"></i>
+							{#if slugs.length - 1 === i}
+								<span class="mx-1 rounded px-1 py-0.5">
+									{name.charAt(0).toUpperCase() + name.slice(1)}
+								</span>
+							{:else}
+								<a href={page} class="mx-1 rounded px-1 py-0.5 hover:bg-white-700 active:bg-white-600">
+									{name.charAt(0).toUpperCase() + name.slice(1)}
+								</a>
+							{/if}
+						</div>
+					{/each}
+				</div>
+			{:else}
+				<div>
+					Error {$page.status}: {$page.error?.message}
+				</div>
+			{/if}
 		</div>
 		{#if navigationHistory.length > 0}
 			<a href={navigationHistory.at(-1)?.url.href} in:fly={{ duration: 600, amount: 10 }}>
