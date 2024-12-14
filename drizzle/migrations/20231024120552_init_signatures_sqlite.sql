514559380e82b08936baf21a280844e02ad87da9
v-rogg <mail@valentinrogg.de>
2024-12-14 14:56:07 +0100

    delete old drizzle data

--- drizzle/migrations/20231024120552_init_signatures_sqlite.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists signatures;
-create table if not exists signatures (
-    id          text primary key not null,
-    name        text   not null,
-    ts_created  text default current_timestamp not null,
-    ts_modified text,
-    approved    integer default null,
-    signature   text   not null
-);
