d18edca2aa9c1f9f88480c5605d0f0a6e35aaa39
v-rogg <mail@valentinrogg.de>
2025-01-12 18:25:14 +0100

    minor layout refactor

--- src/routes/(landing)/+layout.svelte
+++ src/routes/(landing)/+layout.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
 	let { children } = $props();
-	import { page } from "$app/stores";
 	import { appState } from "$lib/stores.svelte";
 
 	appState.metadata = undefined;
 </script>
 
-<main class="container relative mx-auto flex flex-col gap-4" class:m-32={$page.url.pathname !== "/"}>
+<main class="container relative mx-auto flex flex-col gap-4">
 	{@render children()}
 </main>
