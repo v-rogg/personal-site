fea2bf6329914f740abe99b034b20e293127fdda
v-rogg <mail@valentinrogg.de>
2025-03-16 21:17:13 +0100

    Update link to new excalidraw file

--- src/routes/workshop/visionboard/+server.ts
+++ src/routes/workshop/visionboard/+server.ts
@@ -2,7 +2,7 @@ export function GET(): Response {
 	return new Response(null, {
 		status: 301,
 		headers: {
-			location: "https://r2.valentinrogg.de/files/Personal%20Vision-Board.excalidraw"
+			location: "https://r2.valentinrogg.de/files/PersonalVisionBoard.excalidraw"
 		}
 	});
 }
